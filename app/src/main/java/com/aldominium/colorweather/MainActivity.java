8c2b014417ab3c9b2f66d22834ce3796f9ee2118
aldominium <aldo2122@outlook.com>
2016-07-17 17:21:10 -0500

    Adds buttons without style

--- app/src/main/java/com/aldominium/colorweather/MainActivity.java
+++ app/src/main/java/com/aldominium/colorweather/MainActivity.java
@@ -2,12 +2,19 @@ package com.aldominium.colorweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private TextView dailyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
