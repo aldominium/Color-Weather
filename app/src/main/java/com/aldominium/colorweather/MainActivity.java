3e05e6f6cd8d3399a17eaf242238b68006373421
aldominium <aldo2122@outlook.com>
2016-07-23 18:46:47 -0500

    Añade click listener a daily text view

--- app/src/main/java/com/aldominium/colorweather/MainActivity.java
+++ app/src/main/java/com/aldominium/colorweather/MainActivity.java
@@ -2,18 +2,32 @@ package com.aldominium.colorweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
+    private TextView dailyWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dailyWeatherTextView = (TextView) findViewById(R.id.dailyWeatherTextView);
+
+        dailyWeatherTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG,"Click en daily Text View");
+            }
+        });
 
     }
+
+
+
 }
