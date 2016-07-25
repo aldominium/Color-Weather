1d1715de35d37a5653c745b99f767aeba58065ea
aldominium <aldo2122@outlook.com>
2016-07-24 22:30:09 -0500

    Crea el daily adapter

---
+++ app/src/main/java/com/aldominium/colorweather/Adapters/DailyWeatherAdapter.java
@@ -0,0 +1,33 @@
+package com.aldominium.colorweather.Adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by aldo on 24/07/2016.
+ */
+
+public class DailyWeatherAdapter extends BaseAdapter{
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;//No lo usaremos
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
