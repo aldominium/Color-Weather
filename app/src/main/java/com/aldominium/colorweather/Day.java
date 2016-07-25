5e780e2ae8b98d9d5753aa4e8d8d9b71a0fe2e77
aldominium <aldo2122@outlook.com>
2016-07-24 22:22:46 -0500

    Crea clase Day

---
+++ app/src/main/java/com/aldominium/colorweather/Day.java
@@ -0,0 +1,39 @@
+package com.aldominium.colorweather;
+
+/**
+ * Created by aldo on 24/07/2016.
+ */
+
+public class Day {
+
+    private String dayName;
+    private String weatherDescription;
+    private String rainProbability;
+
+    public String getDayName() {
+        return dayName;
+    }
+
+    public void setDayName(String dayName) {
+        this.dayName = dayName;
+    }
+
+    public String getWeatherDescription() {
+        return weatherDescription;
+    }
+
+    public void setWeatherDescription(String weatherDescription) {
+        this.weatherDescription = weatherDescription;
+    }
+
+    public String getRainProbability() {
+        return rainProbability;
+    }
+
+    public void setRainProbability(String rainProbability) {
+        this.rainProbability = rainProbability;
+    }
+
+
+
+}
