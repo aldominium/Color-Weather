d2cd5beb6e68087d5bd68632bf2ac4b40f880618
aldominium <aldo2122@outlook.com>
2016-07-30 23:11:36 -0500

    Creates Hour class

---
+++ app/src/main/java/com/aldominium/colorweather/Hour.java
@@ -0,0 +1,30 @@
+package com.aldominium.colorweather;
+
+/**
+ * Created by aldo on 30/07/2016.
+ */
+
+public class Hour {
+
+    private String title;
+    private String weatherDescription;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWeatherDescription() {
+        return weatherDescription;
+    }
+
+    public void setWeatherDescription(String weatherDescription) {
+        this.weatherDescription = weatherDescription;
+    }
+
+    
+
+}
