6b7511c78720ceed720df1f92ece04e4eb3da2f0
aldominium <aldo2122@outlook.com>
2016-07-30 23:40:52 -0500

    Sets adapter to the activity

--- app/src/main/java/com/aldominium/colorweather/HourlyWeatherActivity.java
+++ app/src/main/java/com/aldominium/colorweather/HourlyWeatherActivity.java
@@ -3,28 +3,40 @@ package com.aldominium.colorweather;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.aldominium.colorweather.Adapters.HourlyWeatherAdapter;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class HourlyWeatherActivity extends Activity {
+
+    @BindView(R.id.hourlyListView) ListView hourlyListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_weather);
+        ButterKnife.bind(this);
 
         Hour hour = new Hour();
         hour.setTitle("11:00pm");
         hour.setWeatherDescription("There is a storm");
 
+        Hour hour2 = new Hour();
+        hour2.setTitle("12:00pm");
+        hour2.setWeatherDescription("There is another storm!");
+
         ArrayList<Hour> hours = new ArrayList<Hour>();
         hours.add(hour);
+        hours.add(hour2);
 
         HourlyWeatherAdapter hourlyWeatherAdapter = new HourlyWeatherAdapter(this,hours);
 
-        
+        hourlyListView.setAdapter(hourlyWeatherAdapter);
 
     }
 }
