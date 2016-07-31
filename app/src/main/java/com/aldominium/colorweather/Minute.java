807b8bcf87ad7c09ccbd228b3da0544074692d2f
aldominium <aldo2122@outlook.com>
2016-07-31 00:09:54 -0500

    creates layout and minute

---
+++ app/src/main/java/com/aldominium/colorweather/Minute.java
@@ -0,0 +1,29 @@
+package com.aldominium.colorweather;
+
+/**
+ * Created by aldo on 31/07/2016.
+ */
+
+public class Minute {
+
+    private String title;
+    private String rainProbability;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRainProbability() {
+        return rainProbability;
+    }
+
+    public void setRainProbability(String rainProbability) {
+        this.rainProbability = rainProbability;
+    }
+
+
+}
