a5c49b6691b6dd6a9973f8c85ffafb478bfcba15
aldominium <aldo2122@outlook.com>
2016-07-31 22:49:51 -0500

    Makes hour parcelable

--- app/src/main/java/com/aldominium/colorweather/Hour.java
+++ app/src/main/java/com/aldominium/colorweather/Hour.java
@@ -1,13 +1,37 @@
 package com.aldominium.colorweather;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by aldo on 30/07/2016.
  */
 
-public class Hour {
+public class Hour implements Parcelable {
 
     private String title;
     private String weatherDescription;
+
+    public Hour(){
+
+    }
+
+    protected Hour(Parcel in) {
+        title = in.readString();
+        weatherDescription = in.readString();
+    }
+
+    public static final Creator<Hour> CREATOR = new Creator<Hour>() {
+        @Override
+        public Hour createFromParcel(Parcel in) {
+            return new Hour(in);
+        }
+
+        @Override
+        public Hour[] newArray(int size) {
+            return new Hour[size];
+        }
+    };
 
     public String getTitle() {
         return title;
@@ -25,6 +49,15 @@ public class Hour {
         this.weatherDescription = weatherDescription;
     }
 
-    
 
+    @Override
+    public int describeContents() {
+        return 0;//We dont use this
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(title);
+        parcel.writeString(weatherDescription);
+    }
 }
