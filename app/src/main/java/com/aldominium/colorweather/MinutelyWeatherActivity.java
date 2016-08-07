fc5bc50a143dc12bfe999c5f4be9a69027669a16
aldominium <aldo2122@outlook.com>
2016-08-06 20:04:25 -0500

    Recycler view no data

--- app/src/main/java/com/aldominium/colorweather/MinutelyWeatherActivity.java
+++ app/src/main/java/com/aldominium/colorweather/MinutelyWeatherActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
 import com.aldominium.colorweather.Adapters.MinutelyWeatherAdapter;
 
@@ -16,6 +18,7 @@ import butterknife.ButterKnife;
 public class MinutelyWeatherActivity extends Activity {
 
     @BindView(R.id.minutelyRecyclerView)RecyclerView minutelyRecyclerView;
+    @BindView(R.id.minutelyNoDataTextView)TextView noDataTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +31,35 @@ public class MinutelyWeatherActivity extends Activity {
         ArrayList<Minute> minutes = intent.getParcelableArrayListExtra(MainActivity.MINUTE_ARRAY_LIST);
 
 
+        if(minutes != null && !minutes.isEmpty()){
+
+            noDataTextView.setVisibility(View.GONE);
+            minutelyRecyclerView.setVisibility(View.VISIBLE);
+
+            MinutelyWeatherAdapter minutelyWeatherAdapter = new MinutelyWeatherAdapter(this,minutes);
+
+            minutelyRecyclerView.setAdapter(minutelyWeatherAdapter);
+
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+
+            minutelyRecyclerView.setLayoutManager(layoutManager);
+
+            minutelyRecyclerView.setHasFixedSize(true);
 
 
-        MinutelyWeatherAdapter minutelyWeatherAdapter = new MinutelyWeatherAdapter(this,minutes);
+        }else {
 
-        minutelyRecyclerView.setAdapter(minutelyWeatherAdapter);
+            //Desplegar nodata textview
+            //volver recyclerview invisible
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+            noDataTextView.setVisibility(View.VISIBLE);
+            minutelyRecyclerView.setVisibility(View.GONE);
 
-        minutelyRecyclerView.setLayoutManager(layoutManager);
 
-        minutelyRecyclerView.setHasFixedSize(true);
+
+
+        }
+
 
 
 
